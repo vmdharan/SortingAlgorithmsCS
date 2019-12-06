adfa4af2d74d87bb8b77b6e47af371694a5e3183
vmdharan <vmdharan@users.noreply.github.com>
2019-12-06 20:41:48 +1100

    Test sorting with multiple threads
    Test sorting with multiple threads.

--- SortingAlgorithmsCS/Program.cs
+++ SortingAlgorithmsCS/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortingAlgorithmsCS
@@ -14,7 +15,9 @@ namespace SortingAlgorithmsCS
     {
         private const int MAXINDEX = 99999;  // Maximum value to use as index (for RNG)
         private const int MAXITEMS = 20000;  // Maximum number of elements in array
+
         private static SortableItem<Car>[] vehicles;
+
 
         static void Main(string[] args)
         {
@@ -26,12 +29,12 @@ namespace SortingAlgorithmsCS
 
             // Run sorting algorithm.
             sw.Start();
-            PerformSort();
+            //PerformSort();
+            PerformSortMultiThreaded();
             sw.Stop();
-            Console.WriteLine("\n-- Elapsed time(ms) for sort --");
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine("\n -- Total Sorting time: " + sw.Elapsed.TotalMilliseconds + " --");
 
-            Console.WriteLine("\n-- After sorting -- ");
+            Console.WriteLine("\n-- After sorting --");
             //ShowData();
 
             Console.ReadKey();
@@ -51,11 +54,52 @@ namespace SortingAlgorithmsCS
 
         private static void PerformSort()
         {
-            //var s1 = new BubbleSort<Car>();
-            var s1 = new SelectionSort<Car>();
-            s1.Sort(ref vehicles);
+            RunSelectionSort(vehicles);
+            RunBubbleSort(vehicles);
+        }
 
-            
+        private static void PerformSortMultiThreaded()
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(RunBubbleSort));
+            t1.Start(vehicles);
+
+            Thread t2 = new Thread(new ParameterizedThreadStart(RunSelectionSort));
+            t2.Start(vehicles);
+
+            t1.Join();
+            t2.Join();
+        }
+
+        private static void RunBubbleSort(Object o)
+        {
+            var vehicles = (SortableItem<Car>[])o;
+            Stopwatch sw = new Stopwatch();
+            var s1 = new BubbleSort<Car>();
+
+            sw.Start();
+            s1.Sort(ref vehicles);
+            sw.Stop();
+
+            ShowElapsedTime(sw.Elapsed.TotalMilliseconds, "BubbleSort");
+        }
+
+        private static void RunSelectionSort(Object o)
+        {
+            var vehicles = (SortableItem<Car>[])o;
+            Stopwatch sw = new Stopwatch();
+            var s1 = new SelectionSort<Car>();
+
+            sw.Start();
+            s1.Sort(ref vehicles);
+            sw.Stop();
+
+            ShowElapsedTime(sw.Elapsed.TotalMilliseconds, "SelectionSort");
+        }
+
+        private static void ShowElapsedTime(double t, string sortType)
+        {
+            Console.WriteLine("\n-- Elapsed time(ms) for " + sortType + " --");
+            Console.WriteLine(t);
         }
 
         private static void ShowData()
